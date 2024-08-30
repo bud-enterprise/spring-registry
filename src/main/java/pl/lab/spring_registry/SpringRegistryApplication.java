5cb2714c07bf81688727f75e4b62ffd653b748c3
bartek <bartoszbud@gmail.com>
2024-08-30 21:25:29 +0200

    initial commit

---
+++ src/main/java/pl/lab/spring_registry/SpringRegistryApplication.java
@@ -0,0 +1,13 @@
+package pl.lab.spring_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lab/spring_registry/SpringRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lab.spring_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
